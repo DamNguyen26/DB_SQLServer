ebd7ae53c2367dbc79575c748e958d3c443417fe
Tungle-2512 <tungblack2512@users.noreply.github.com>
2021-01-10 19:41:58 +0700

    updatedoantot

---
+++ BaiTapCuoiKy/QuanLyDoAn/QuanLyDoAn/Lop.Designer.cs
@@ -0,0 +1,413 @@
+﻿
+namespace QuanLyDoAn
+{
+    partial class Lop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lop));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbLop = new System.Windows.Forms.TextBox();
+            this.tbHoTen = new System.Windows.Forms.TextBox();
+            this.tbMSSV = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbLop = new System.Windows.Forms.CheckBox();
+            this.cbHoTen = new System.Windows.Forms.CheckBox();
+            this.cbMSSV = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btIn = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.btCapNhat = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.btTim = new System.Windows.Forms.Button();
+            this.btLamMoi = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btLamMoi);
+            this.groupBox1.Controls.Add(this.tbLop);
+            this.groupBox1.Controls.Add(this.tbHoTen);
+            this.groupBox1.Controls.Add(this.tbMSSV);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(367, 230);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin lớp";
+            // 
+            // tbLop
+            // 
+            this.tbLop.Location = new System.Drawing.Point(116, 196);
+            this.tbLop.Margin = new System.Windows.Forms.Padding(4);
+            this.tbLop.Name = "tbLop";
+            this.tbLop.Size = new System.Drawing.Size(87, 22);
+            this.tbLop.TabIndex = 12;
+            // 
+            // tbHoTen
+            // 
+            this.tbHoTen.Location = new System.Drawing.Point(116, 58);
+            this.tbHoTen.Margin = new System.Windows.Forms.Padding(4);
+            this.tbHoTen.Name = "tbHoTen";
+            this.tbHoTen.Size = new System.Drawing.Size(132, 22);
+            this.tbHoTen.TabIndex = 7;
+            // 
+            // tbMSSV
+            // 
+            this.tbMSSV.Location = new System.Drawing.Point(116, 25);
+            this.tbMSSV.Margin = new System.Windows.Forms.Padding(4);
+            this.tbMSSV.Name = "tbMSSV";
+            this.tbMSSV.Size = new System.Drawing.Size(132, 22);
+            this.tbMSSV.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 196);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Số SV:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 161);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Bậc đào tạo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã GV:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã lớp:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btTim);
+            this.groupBox2.Controls.Add(this.cbLop);
+            this.groupBox2.Controls.Add(this.cbHoTen);
+            this.groupBox2.Controls.Add(this.cbMSSV);
+            this.groupBox2.Location = new System.Drawing.Point(414, 13);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(276, 230);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chọn thông tin tìm kiếm";
+            // 
+            // cbLop
+            // 
+            this.cbLop.AutoSize = true;
+            this.cbLop.Location = new System.Drawing.Point(27, 98);
+            this.cbLop.Margin = new System.Windows.Forms.Padding(4);
+            this.cbLop.Name = "cbLop";
+            this.cbLop.Size = new System.Drawing.Size(120, 21);
+            this.cbLop.TabIndex = 2;
+            this.cbLop.Text = "Tìm theo khóa";
+            this.cbLop.UseVisualStyleBackColor = true;
+            // 
+            // cbHoTen
+            // 
+            this.cbHoTen.AutoSize = true;
+            this.cbHoTen.Location = new System.Drawing.Point(27, 62);
+            this.cbHoTen.Margin = new System.Windows.Forms.Padding(4);
+            this.cbHoTen.Name = "cbHoTen";
+            this.cbHoTen.Size = new System.Drawing.Size(132, 21);
+            this.cbHoTen.TabIndex = 1;
+            this.cbHoTen.Text = "Tìm theo tên lớp";
+            this.cbHoTen.UseVisualStyleBackColor = true;
+            // 
+            // cbMSSV
+            // 
+            this.cbMSSV.AutoSize = true;
+            this.cbMSSV.Location = new System.Drawing.Point(27, 25);
+            this.cbMSSV.Margin = new System.Windows.Forms.Padding(4);
+            this.cbMSSV.Name = "cbMSSV";
+            this.cbMSSV.Size = new System.Drawing.Size(131, 21);
+            this.cbMSSV.TabIndex = 0;
+            this.cbMSSV.Text = "Tìm theo mã lớp";
+            this.cbMSSV.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btIn);
+            this.groupBox3.Controls.Add(this.btThoat);
+            this.groupBox3.Controls.Add(this.btCapNhat);
+            this.groupBox3.Controls.Add(this.btXoa);
+            this.groupBox3.Controls.Add(this.btThem);
+            this.groupBox3.Location = new System.Drawing.Point(13, 251);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(677, 87);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Các chức năng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Tên lớp:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 89);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 128);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Khóa đào tạo:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 161);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 17;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "62",
+            "61",
+            "60",
+            "59",
+            "58",
+            "57"});
+            this.comboBox1.Location = new System.Drawing.Point(116, 125);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 357);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(689, 158);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // btIn
+            // 
+            this.btIn.Image = ((System.Drawing.Image)(resources.GetObject("btIn.Image")));
+            this.btIn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btIn.Location = new System.Drawing.Point(388, 39);
+            this.btIn.Margin = new System.Windows.Forms.Padding(4);
+            this.btIn.Name = "btIn";
+            this.btIn.Size = new System.Drawing.Size(100, 28);
+            this.btIn.TabIndex = 4;
+            this.btIn.Text = "Xuất";
+            this.btIn.UseVisualStyleBackColor = true;
+            // 
+            // btThoat
+            // 
+            this.btThoat.Image = ((System.Drawing.Image)(resources.GetObject("btThoat.Image")));
+            this.btThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btThoat.Location = new System.Drawing.Point(512, 39);
+            this.btThoat.Margin = new System.Windows.Forms.Padding(4);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(100, 28);
+            this.btThoat.TabIndex = 3;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            // 
+            // btCapNhat
+            // 
+            this.btCapNhat.Image = ((System.Drawing.Image)(resources.GetObject("btCapNhat.Image")));
+            this.btCapNhat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btCapNhat.Location = new System.Drawing.Point(247, 39);
+            this.btCapNhat.Margin = new System.Windows.Forms.Padding(4);
+            this.btCapNhat.Name = "btCapNhat";
+            this.btCapNhat.Size = new System.Drawing.Size(119, 28);
+            this.btCapNhat.TabIndex = 2;
+            this.btCapNhat.Text = "Cập Nhật";
+            this.btCapNhat.UseVisualStyleBackColor = true;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Image = ((System.Drawing.Image)(resources.GetObject("btXoa.Image")));
+            this.btXoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btXoa.Location = new System.Drawing.Point(129, 39);
+            this.btXoa.Margin = new System.Windows.Forms.Padding(4);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(100, 28);
+            this.btXoa.TabIndex = 1;
+            this.btXoa.Text = "Xóa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            // 
+            // btThem
+            // 
+            this.btThem.Image = ((System.Drawing.Image)(resources.GetObject("btThem.Image")));
+            this.btThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btThem.Location = new System.Drawing.Point(13, 39);
+            this.btThem.Margin = new System.Windows.Forms.Padding(4);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(100, 28);
+            this.btThem.TabIndex = 0;
+            this.btThem.Text = "Thêm";
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // btTim
+            // 
+            this.btTim.Image = ((System.Drawing.Image)(resources.GetObject("btTim.Image")));
+            this.btTim.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btTim.Location = new System.Drawing.Point(137, 194);
+            this.btTim.Margin = new System.Windows.Forms.Padding(4);
+            this.btTim.Name = "btTim";
+            this.btTim.Size = new System.Drawing.Size(100, 27);
+            this.btTim.TabIndex = 3;
+            this.btTim.Text = "Tìm";
+            this.btTim.UseVisualStyleBackColor = true;
+            // 
+            // btLamMoi
+            // 
+            this.btLamMoi.Image = ((System.Drawing.Image)(resources.GetObject("btLamMoi.Image")));
+            this.btLamMoi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btLamMoi.Location = new System.Drawing.Point(247, 194);
+            this.btLamMoi.Margin = new System.Windows.Forms.Padding(4);
+            this.btLamMoi.Name = "btLamMoi";
+            this.btLamMoi.Size = new System.Drawing.Size(112, 28);
+            this.btLamMoi.TabIndex = 13;
+            this.btLamMoi.Text = "Làm mới";
+            this.btLamMoi.UseVisualStyleBackColor = true;
+            // 
+            // Lop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 516);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Lop";
+            this.Text = "Quan ly lop";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btLamMoi;
+        private System.Windows.Forms.TextBox tbLop;
+        private System.Windows.Forms.TextBox tbHoTen;
+        private System.Windows.Forms.TextBox tbMSSV;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btTim;
+        private System.Windows.Forms.CheckBox cbLop;
+        private System.Windows.Forms.CheckBox cbHoTen;
+        private System.Windows.Forms.CheckBox cbMSSV;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btIn;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Button btCapNhat;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
