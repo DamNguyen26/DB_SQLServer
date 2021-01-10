c329182872b3fbfc260b8753de72e852eeda80b6
Dam Nguyen <dammit2525@gmail.com>
2021-01-10 14:07:44 +0700

    update

--- BaiTapCuoiKy/SQLQuery_BaiTap4.sql
+++ BaiTapCuoiKy/SQLQuery_BaiTap4.sql
@@ -1,55 +1,89 @@
 CREATE DATABASE QLDoAn
 CREATE TABLE tblTaiKhoan(
-    MaSV, KEY
-    Email,
-    Pass
+    MaSV NOT NULL PRIMARY KEY,
+    Email NOT NULL,
+    Pass NOT NULL,
+    FOREIGN KEY (MaSV) REFERENCES tblSV(MaSV)
 ) 
 CREATE TABLE tblSV(
-    MaSV, KEY
-    MaLop, KEY
-    TenSV, 
-    NamSinh, 
-    Email, 
-    SDT 
+    MaSV NOT NULL,
+    MaLop NOT NULL,
+    PRIMARY KEY(
+        MaSV, 
+        MaLop
+    ),
+    TenSV NOT NULL, 
+    NamSinh NOT NULL, 
+    Email NOT NULL, 
+    SDT NOT NULL,
+    FOREIGN KEY (MaLop) REFERENCES tblLop(MaLop)
 )
 CREATE TABLE tblLop(
-    MaLop, KEY
-    MaGV, KEY
-    TenLop,
-    KhoaDaoTao,
-    BacDaoTao,
-    SoSV
+    MaLop NOT NULL,
+    MaGV NOT NULL,
+    PRIMARY KEY(
+        MaLop, 
+        MaGV
+    ),
+    TenLop NOT NULL,
+    KhoaDaoTao NOT NULL,
+    BacDaoTao NOT NULL,
+    SoSV NOT NULL,
+    FOREIGN KEY (MaGV) REFERENCES tblGV(MaGV)
 )
 CREATE TABLE tblGV(
-    MaGV, 
-    TenGV, 
-    ViTriDay, 
-    Email, 
-    SDT, 
-    HocVi
+    MaGV NOT NULL PRIMARY KEY, 
+    TenGV NOT NULL, 
+    ViTriDay NOT NULL, 
+    Email NOT NULL, 
+    SDT NOT NULL, 
+    HocVi NOT NULL
 )
 CREATE TABLE tblHoiDong(
-    MaHoiDong, KEY
-    ChuTichHD, 
-    SoLuong
+    MaHoiDong NOT NULL PRIMARY KEY,
+    ChuTichHD NOT NULL, 
+    SoLuong NOT NULL
 )
 CREATE TABLE tblDoAn(
-    MaDoAn, KEY
-    MaHoiDong, KEY
-    TenDoAn, 
-    Nam 
+    MaDoAn NOT NULL,
+    MaHoiDong NOT NULL,
+    PRIMARY KEY(
+        MaDoAn, 
+        MaHoiDong
+    ),
+    TenDoAn NOT NULL, 
+    Nam NOT NULL,
+    FOREIGN KEY (MaHoiDong) REFERENCES tblHoiDong(MaHoiDong)
 )
 CREATE TABLE tblDoAn_SV(
-    MaSV, 
-    MaGV,
-    MaDoAn, 
-    Diem
+    MaSV NOT NULL, 
+    MaLop NOT NULL,
+    MaDoAn NOT NULL, 
+    PRIMARY KEY(
+        MaSV, 
+        MaLop,
+        MaDoAn
+    ),
+    Diem NOT NULL,
+    FOREIGN KEY (MaSV) REFERENCES tblSV(MaSV),
+    FOREIGN KEY (MaLop) REFERENCES tblLop(MaLop),
+    FOREIGN KEY (MaDoAn) REFERENCES tblDoAn(MaDoAn)
 )
-
-
-
-
-
-
-
-
+insert into tblTaiKhoan(MaSV, Email, Pass) values
+    ('a', 'b', 'c'),
+    ('a', 'b', 'c'),
+    ('a', 'b', 'c'),
+    ('a', 'b', 'c'),
+    ('a', 'b', 'c');
+insert into tblSV(MaSV, MaLop, TenSV, NamSinh, Email, SDT) values
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f');
+insert into tblLop(MaLop, MaGV, TenLop, KhoaDaoTao, BacDaoTao, SoSV) values
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f'),
+    ('a', 'b', 'c', 'd', 'e', 'f');
